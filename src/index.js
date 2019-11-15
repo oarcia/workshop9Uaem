ba5364c201a466b0e3d71bdc7383dfd855c4a007
Ubuntu <ubuntu@ip-172-31-44-212.ec2.internal>
2019-11-15 17:50:44 +0000

    Workshop 0  9 semestre

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const SlackBots =require('slackbots');
+const axios = require('axios');
+
+const bot= new SlackBots({
+    token:'xoxb-823641805906-838297832791-J6SZgLmCqVHH6MYalfhB12xz',
+    name:'botEcatepunk'
+});
+
+bot.on('open',()=>console.log('mi bot esta vivo'));
+
+bot.on('start',()=>{
+    bot.postMessageToChannel('general',"esta vivo el bot :smile: ");
+});
+
+bot.on('message',async(data)=>{
+    if(data.type !=='message' || data.subtype == 'bot_message'
+    || !data.text) return
+    const args = data.text.split(" ");
+    const command = args.splice(1, 1)[0];
+    const user_ide = args.splice(0, 1)[0];
+    const params = args.join(' ');
+    console.log(command,params);
+
+    const res = await axios.get(`http://www.omdbapi.com/?t=${params}&apikey=23ae1e76`);
+    if(res.data.Response === 'False') return bot.postMessageToChannel('general','movie not found');
+    bot.postMessageToChannel('general', `${res.data.Title}: ${res.data.Poster}`);
+    console.log(res.data);
+});
